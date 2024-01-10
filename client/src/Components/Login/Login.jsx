4020e25f4745ebd599a3b62646917a327eac3784
sandeep <sandeepkrpatel2002@gmail.com>
2024-01-10 23:15:32 +0530

    profile style make perfect

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -8,68 +8,68 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const dispatch = useDispatch();
+  const [email, setEmail] = useState("admin@gmail.com");
+  const [password, setPassword] = useState("admin@123456");
+  const dispatch = useDispatch();
 
-	const { error } = useSelector((state) => state.user);
-	const { message } = useSelector((state) => state.like);
+  const { error } = useSelector((state) => state.user);
+  const { message } = useSelector((state) => state.like);
 
-	const loginHandler = (e) => {
-		e.preventDefault();
-		dispatch(loginUser(email, password));
-	};
+  const loginHandler = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(email, password));
+  };
 
-	useEffect(() => {
-		if (error) {
-			toast.error(error, {
-				toastId: "error1",
-			});
-			dispatch({ type: "clearErrors" });
-		}
+  useEffect(() => {
+    if (error) {
+      toast.error(error, {
+        toastId: "error1",
+      });
+      dispatch({ type: "clearErrors" });
+    }
 
-		if (message) {
-			toast.success(message, {
-				toastId: "success1",
-			});
-			dispatch({ type: "clearMessage" });
-		}
-	}, [error, dispatch, message]);
-	return (
-		<div className="login">
-			<form className="loginForm" onSubmit={loginHandler}>
-				<Typography variant="h3" style={{ padding: "2rem" }}>
-					Social App
-				</Typography>
-				<input
-					type="email"
-					placeholder="Email"
-					required
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					required
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<Link to="/forgot/password">
-					<Typography>Forgot Password?</Typography>
-				</Link>
-				<Button type="submit">Login</Button>
-				<Link to="/register">
-					<Typography>New User?</Typography>
-				</Link>
-			</form>
-			<ToastContainer
-				position="top-center"
-				hideProgressBar={true}
-				theme="dark"
-			/>
-		</div>
-	);
+    if (message) {
+      toast.success(message, {
+        toastId: "success1",
+      });
+      dispatch({ type: "clearMessage" });
+    }
+  }, [error, dispatch, message]);
+  return (
+    <div className="login">
+      <form className="loginForm" onSubmit={loginHandler}>
+        <Typography variant="h3" style={{ padding: "2rem" }}>
+          Social App
+        </Typography>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Link to="/forgot/password">
+          <Typography>Forgot Password?</Typography>
+        </Link>
+        <Button type="submit">Login</Button>
+        <Link to="/register">
+          <Typography>New User?</Typography>
+        </Link>
+      </form>
+      <ToastContainer
+        position="top-center"
+        hideProgressBar={true}
+        theme="dark"
+      />
+    </div>
+  );
 };
 
 export default Login;
